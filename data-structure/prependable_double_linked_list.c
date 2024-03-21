8bd6930631d0b4c323ead6752c394b1c9b63913f
pouryakillsound <pouryakillsound@gmail.coms>
2024-03-21 12:46:57 +0330

    adding comments to prependable_double_linked_list.c and replacing malloc with a calloc for the sake of differentiality and also for coding two lines less

--- data-structure/prependable_double_linked_list.c
+++ data-structure/prependable_double_linked_list.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define N 2
+#define N 2 // length of list
 
 typedef struct node
 {
-        struct node *next, *prev;
+        struct node *next, *prev; // two pointer one to the next elemenet and one to the previous element of the list
         int data;
 }
 node;
 
 
-void printf_forward(node *list)
+void printf_forward(node *list) // function for printing it from the first element to the last one
 {
         for(node *ptr = list; ptr; ptr = ptr->next)
         {        
@@ -24,12 +24,14 @@ void printf_forward(node *list)
 
 }
 
-void printf_backward(node *list)
+void printf_backward(node *list) // function for printing it from last one element to the the first in reversed direction
 {
         node *ptr = list;
+        /* This while loop will set the ptr to the last element */
         while (ptr->next)
                 ptr = ptr->next;
-        for (; ptr; ptr = ptr->prev)
+
+        for (; ptr; ptr = ptr->prev) // Moving backwards
         {
                 printf("%i", ptr->data);
                 if (ptr->prev)
@@ -39,7 +41,7 @@ void printf_backward(node *list)
 
 }
 
-bool free_all(node *list)
+bool free_all(node *list) // Function for free all malloced elements 
 {
         if (!list) return false;
         node *ptr = list;
@@ -50,14 +52,13 @@ bool free_all(node *list)
 
 int main()
 {
-        node *list = NULL;
-        int data[N] = {2, 3};
+        node *list = NULL; // Initializing list pointer to NULL
+        int data[N] = {2, 3}; // Example data
+        /* This for loop is a typical prepending */
         for (int i=0; i < N; i++)
         {
-                node *n = malloc(sizeof(node));
+                node *n = calloc(1, sizeof(node)); // using calloc because it sets all bytes to 0 so the pointers prev and next would be set to zero and we dont have to do it by hand
                 n->data = data[i];
-                n->next = NULL;
-                n->prev = NULL;
                 if (!list) list = n;
                 else
                 {
@@ -66,10 +67,10 @@ int main()
                         list = n;
                 }
         }
-        
+        /* Printing them both ways to see the difference*/
         printf_backward(list); // This shows an appending format 
         printf("-------\n");
         printf_forward(list); // This shows a prepending format
-        free_all(list);
+        free_all(list); // Freeing memory
         return 0;
 }
